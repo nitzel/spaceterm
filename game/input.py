e6b93420f5ca8548e30db0550b530e61638e1b12
Diego Malatesta <diego.malatesta@gmail.com>
2015-02-18 18:49:54 +0000

    Code styling for input.py

--- game/input.py
+++ game/input.py
@@ -1,33 +1,31 @@
-import os
-import platform
-import curses
-
 class Input:
-  """Manages player input"""
-  def __init__(self):
-    super(Input, self).__init__()
-    self.callbacks = {}
 
-  def getPlayerInput(self, screen):
-    """Standardized method to get player input"""
-    ch = screen.get_wch()
-    self.runCallbacks(ch)
-    return ch
+    """Manages player input"""
 
-  def onKey(self, ch, callback):
-    """Binds a callback to a keypress"""
-    if ch not in self.callbacks:
-      self.callbacks[ch] = []
+    def __init__(self):
+        super(Input, self).__init__()
+        self.callbacks = {}
 
-    self.callbacks[ch].append(callback)
+    def getPlayerInput(self, screen):
+        """Standardized method to get player input"""
+        ch = screen.get_wch()
+        self.runCallbacks(ch)
+        return ch
 
-  def onKeys(self, chs, callback):
-    """Wrapper on onKey to add multiple bindings in one call"""
-    for ch in chs:
-      self.onKey(ch, callback)
+    def onKey(self, ch, callback):
+        """Binds a callback to a keypress"""
+        if ch not in self.callbacks:
+            self.callbacks[ch] = []
 
-  def runCallbacks(self, ch):
-    """Run callback functions after a key is pressed"""
-    if ch in self.callbacks:
-      for callback in self.callbacks[ch]:
-        callback()
+        self.callbacks[ch].append(callback)
+
+    def onKeys(self, chs, callback):
+        """Wrapper on onKey to add multiple bindings in one call"""
+        for ch in chs:
+            self.onKey(ch, callback)
+
+    def runCallbacks(self, ch):
+        """Run callback functions after a key is pressed"""
+        if ch in self.callbacks:
+            for callback in self.callbacks[ch]:
+                callback()
