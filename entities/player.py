2a042a8e1270d83119ca69b1ca4fe6408f45a0ab
Diego Malatesta <diego.malatesta@gmail.com>
2015-02-18 18:50:45 +0000

    Code styling for player.py

--- entities/player.py
+++ entities/player.py
@@ -1,56 +1,59 @@
 from game.coordinates import Coordinates
 
+
 class Player:
-  """The player object manages the current player"""
-  name = ''
-  coordinates = Coordinates(0, 0, 0)
 
-  @staticmethod
-  def setName(name):
-    Player.name = name
+    """The player object manages the current player"""
 
-  @staticmethod
-  def getCoordinates():
-    """Returns the coordinates object with player position"""
-    return Player.coordinates
+    name = ''
+    coordinates = Coordinates(0, 0, 0)
 
-  @staticmethod
-  def moveTo(coordinates):
-    """Moves the player to new coordinates"""
-    Player.coordinates = coordinates
+    @staticmethod
+    def setName(name):
+        Player.name = name
 
-  @staticmethod
-  def moveToTop():
-    Player.coordinates.addY(-1)
+    @staticmethod
+    def getCoordinates():
+        """Returns the coordinates object with player position"""
+        return Player.coordinates
 
-  @staticmethod
-  def moveToTopRight():
-    Player.coordinates.addY(-1)
-    Player.coordinates.addX(1)
+    @staticmethod
+    def moveTo(coordinates):
+        """Moves the player to new coordinates"""
+        Player.coordinates = coordinates
 
-  @staticmethod
-  def moveToRight():
-    Player.coordinates.addX(1)
+    @staticmethod
+    def moveToTop():
+        Player.coordinates.addY(-1)
 
-  @staticmethod
-  def moveToBottomRight():
-    Player.coordinates.addY(1)
-    Player.coordinates.addX(1)
+    @staticmethod
+    def moveToTopRight():
+        Player.coordinates.addY(-1)
+        Player.coordinates.addX(1)
 
-  @staticmethod
-  def moveToBottom():
-    Player.coordinates.addY(1)
+    @staticmethod
+    def moveToRight():
+        Player.coordinates.addX(1)
 
-  @staticmethod
-  def moveToBottomLeft():
-    Player.coordinates.addY(1)
-    Player.coordinates.addX(-1)
+    @staticmethod
+    def moveToBottomRight():
+        Player.coordinates.addY(1)
+        Player.coordinates.addX(1)
 
-  @staticmethod
-  def moveToLeft():
-    Player.coordinates.addX(-1)
+    @staticmethod
+    def moveToBottom():
+        Player.coordinates.addY(1)
 
-  @staticmethod
-  def moveToTopLeft():
-    Player.coordinates.addY(-1)
-    Player.coordinates.addX(-1)
+    @staticmethod
+    def moveToBottomLeft():
+        Player.coordinates.addY(1)
+        Player.coordinates.addX(-1)
+
+    @staticmethod
+    def moveToLeft():
+        Player.coordinates.addX(-1)
+
+    @staticmethod
+    def moveToTopLeft():
+        Player.coordinates.addY(-1)
+        Player.coordinates.addX(-1)
