44bc305d0f6015dc7e5176aabe74529f6228d3a8
Diego Malatesta <diego.malatesta@gmail.com>
2015-02-18 18:41:09 +0000

    Code styling for renderer.py

--- game/renderer.py
+++ game/renderer.py
@@ -1,27 +1,28 @@
-import platform
-import os
 import curses
 from game.matrix import Matrix
 
+
 class Renderer:
-  """This objects manages the rendering phase of the game"""
-  def __init__(self, screen):
-    super(Renderer, self).__init__()
-    self.screen = screen
-    curses.curs_set(0)
-  
-  def render(self, matrix):
-    # Some type checking
-    if not isinstance(matrix, Matrix):
-      raise TypeError
 
-    self.updater(matrix.render())
+    """This objects manages the rendering phase of the game"""
 
-  def updater(self, output):
-    """Updates the buffer with current game status"""
-    self.clearScreen()
-    self.screen.addstr(output)
-    self.screen.refresh()
+    def __init__(self, screen):
+        super(Renderer, self).__init__()
+        self.screen = screen
+        curses.curs_set(0)
 
-  def clearScreen(self):
-    self.screen.erase()
+    def render(self, matrix):
+        # Some type checking
+        if not isinstance(matrix, Matrix):
+            raise TypeError
+
+        self.updater(matrix.render())
+
+    def updater(self, output):
+        """Updates the buffer with current game status"""
+        self.clearScreen()
+        self.screen.addstr(output)
+        self.screen.refresh()
+
+    def clearScreen(self):
+        self.screen.erase()
