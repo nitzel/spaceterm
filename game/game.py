983fa60de13f3b7bf18299ecbe1c30e9b0e526d6
Diego Malatesta <diego.malatesta@gmail.com>
2015-02-18 18:47:22 +0000

    Code styling for game.py

--- game/game.py
+++ game/game.py
@@ -2,53 +2,64 @@ from entities.player import Player
 from game.renderer import Renderer
 from game.input import Input
 from game.matrix import Matrix
-from game.coordinates import Coordinates
+import curses
+
 
 class Game:
-  """The game controller"""
-  def __init__(self, screen):
-    super(Game, self).__init__()
-    self.renderer = None
-    self.inputProcessor = None
-    self.gameMatrix = None
-    self.screen = screen
-    self.runGame = True
 
-  def initialize(self, playerName):
-    # Initialize Player
-    Player.setName(playerName)
+    """The game controller"""
 
-    # Initialize I/O
-    self.renderer = Renderer(self.screen)
-    self.inputProcessor = Input()
-    self.gameMatrix = Matrix(80, 24, 10)
-    self.renderer.clearScreen()
+    def __init__(self, screen):
+        super(Game, self).__init__()
+        self.renderer = None
+        self.inputProcessor = None
+        self.gameMatrix = None
+        self.screen = screen
+        self.runGame = True
 
-    # Bind keys to actions
-    self.inputProcessor.onKey('q', self.quit)
-    self.inputProcessor.onKeys('h', Player.moveToLeft)
-    self.inputProcessor.onKeys('k', Player.moveToTop)
-    self.inputProcessor.onKeys('j', Player.moveToBottom)
-    self.inputProcessor.onKeys('l', Player.moveToRight)
-    self.inputProcessor.onKeys('y', Player.moveToTopLeft)
-    self.inputProcessor.onKeys('u', Player.moveToTopRight)
-    self.inputProcessor.onKeys('b', Player.moveToBottomLeft)
-    self.inputProcessor.onKeys('n', Player.moveToBottomRight)
+    def initialize(self, playerName):
+        # Initialize Player
+        Player.setName(playerName)
 
-  def loop(self):
-    self.gameMatrix.updateLevel()
-    self.gameMatrix.positionPlayer()
-    self.renderer.render(self.gameMatrix)
+        # Initialize I/O
+        self.renderer = Renderer(self.screen)
+        self.inputProcessor = Input()
+        self.gameMatrix = Matrix(80, 24, 10)
+        self.renderer.clearScreen()
 
-    while (self.runGame):
-      self.inputProcessor.getPlayerInput(self.screen)
-      self.gameMatrix.updateLevel()
-      self.gameMatrix.positionPlayer()
-      self.renderer.render(self.gameMatrix)
+        # Bind keys to actions
+        self.inputProcessor.onKey('q', self.quit)
+        self.inputProcessor.onKeys(['h', curses.KEY_LEFT], Player.moveToLeft)
+        self.inputProcessor.onKeys(['k', curses.KEY_UP], Player.moveToTop)
+        self.inputProcessor.onKeys(['j', curses.KEY_DOWN], Player.moveToBottom)
+        self.inputProcessor.onKeys(['l', curses.KEY_RIGHT], Player.moveToRight)
+        self.inputProcessor.onKeys(['y', curses.KEY_A1], Player.moveToTopLeft)
+        self.inputProcessor.onKeys(['u', curses.KEY_A3], Player.moveToTopRight)
+        self.inputProcessor.onKeys(['b', curses.KEY_C1], Player.moveToBottomLeft)
+        self.inputProcessor.onKeys(['n', curses.KEY_C3], Player.moveToBottomRight)
+        self.inputProcessor.onKeys(['.', curses.KEY_B2], self.skipTurn)
 
-  def quit(self):
-    """Quits the game"""
-    self.runGame = False
+        # Initialize curses screen
+        self.screen.border(0, 0, 0, 0, 0, 0, 0, 0)
 
-    # Some cleanup before exiting..
-    self.renderer.clearScreen()
+    def loop(self):
+        self.gameMatrix.updateLevel()
+        self.gameMatrix.positionPlayer()
+        self.renderer.render(self.gameMatrix)
+
+        while (self.runGame):
+            self.inputProcessor.getPlayerInput(self.screen)
+            self.gameMatrix.updateLevel()
+            self.gameMatrix.positionPlayer()
+            self.renderer.render(self.gameMatrix)
+
+    def quit(self):
+        """Quits the game"""
+        self.runGame = False
+
+        # Some cleanup before exiting..
+        self.renderer.clearScreen()
+
+    def skipTurn(self):
+        """Skips player turn"""
+        pass
